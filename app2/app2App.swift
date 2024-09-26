51d10f8fd37ca53874e6c5b70ded6fd04f4d455e
reem <reemyousef.cs@gmail.com>
2024-09-26 12:56:13 +0300

    Initial Commit

---
+++ app2/app2App.swift
@@ -0,0 +1,17 @@
+//
+//  app2App.swift
+//  app2
+//
+//  Created by Reem on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct app2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
